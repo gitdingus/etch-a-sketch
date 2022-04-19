090f5b7e81675c31c1034798861d33c05b515b3d
Josh S <aaabccc87@protonmail.com>
2022-04-19 17:43:42 -0400

    Add functionality to enter custom grid size

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
 const clearButton = document.querySelector("#clear-button");
+const gridButton = document.querySelector("#grid-button");
+const gridInput = document.querySelector("#grid-input");
 const sketchContainer = document.querySelector("#sketch-container");
-const SQUARE = 16;
 
 clearButton.addEventListener('click', clearPanel);
 
-for (let i = 0; i < SQUARE; i++){
-    let outerDiv = document.createElement("div");
-    outerDiv.classList.add("row");
-    for (let j = 0; j < SQUARE; j++){
-        let innerDiv = document.createElement("div");
-        
-        innerDiv.classList.add("column");
+gridButton.addEventListener('click', () => {
+    let input = Number(gridInput.value);
 
-        innerDiv.addEventListener('mouseover', (e) => changeBg(e));
-        outerDiv.appendChild(innerDiv);
+    if (Number.isInteger(input) === true){
+        if (input >=16 && input <= 100){
+            drawGrid(input);
+        }
+        else {
+            alert("Grid size must be between 16 and 100");
+        }
+    }
+    else{
+        alert("Must input whole number between 16 and 100");
     }
 
-    sketchContainer.appendChild(outerDiv);
+    console.log(typeof input);
+})
+
+drawGrid(16); //default grid size 
+
+function drawGrid(size){
+
+    clearGrid();
+    for (let i = 0; i < size; i++){
+        let outerDiv = document.createElement("div");
+        outerDiv.classList.add("row");
+        for (let j = 0; j < size; j++){
+            let innerDiv = document.createElement("div");
+            
+            innerDiv.classList.add("column");
+    
+            innerDiv.addEventListener('mouseover', (e) => changeBg(e));
+            outerDiv.appendChild(innerDiv);
+        }
+    
+        sketchContainer.appendChild(outerDiv);
+    }
+}
+
+function clearGrid(){
+    let children = sketchContainer.childNodes;
+
+    while(children.length != 0){
+        sketchContainer.removeChild(children[0]);
+    }
+    
 }
 
 function clearPanel(){
@@ -26,6 +60,7 @@ function clearPanel(){
         pixel.style.backgroundColor = "white";
     }
 }
+
 function changeBg(event, color = 'black'){
     event.target.style.backgroundColor = color;
 }
