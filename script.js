b217cb67f92689b5786c62c0df4172232cb8ecc1
Josh S <aaabccc87@protonmail.com>
2022-04-19 16:59:15 -0400

    Clear button functional

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const clearButton = document.querySelector("#clear-button");
 const sketchContainer = document.querySelector("#sketch-container");
 const SQUARE = 16;
+
+clearButton.addEventListener('click', clearPanel);
 
 for (let i = 0; i < SQUARE; i++){
     let outerDiv = document.createElement("div");
@@ -16,6 +19,14 @@ for (let i = 0; i < SQUARE; i++){
     sketchContainer.appendChild(outerDiv);
 }
 
+function clearPanel(){
+    const pixels = document.querySelectorAll(".column");
+
+    console.log('Clear button clicked');
+    for (let pixel of pixels){
+        pixel.style.backgroundColor = "white";
+    }
+}
 function changeBg(event, color = 'black'){
     event.target.style.backgroundColor = color;
 }
